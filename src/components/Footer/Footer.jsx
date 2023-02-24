f8154d2d2319bbc778d25af9ba083b0a51686cd6
roniy68 <ahmed.11900449@lpu.in>
2023-02-24 16:19:00 +0530

    [🔥] Works the whole website
    ✅ React-Router Dom [without style]

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './Footer.css';
+import { BsFillHeartFill } from 'react-icons/bs';
 
-const Footer = () => (
-  <div className="footer">
-    &copy;Designed by Microverse
-  </div>
-);
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container row">
+        <p className="text">
+          Built with
+          <BsFillHeartFill className="heart-icon" />
+          by:
+          <a
+            href="https://github.com/roniy68"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ahmed Hasan Rony
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
